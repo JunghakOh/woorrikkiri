3b9e84a90f5201499f569a788db65dbc3bcc7385
Minju0330 <alswnlee0330@gmail.com>
2020-09-02 14:54:41 +0900

    point mento

--- woorrikkiri/accounts/admin.py
+++ woorrikkiri/accounts/admin.py
@@ -9,14 +9,14 @@ User = get_user_model()
 
 class ProfileInline(admin.StackedInline): # 로또 프로젝트에서 썼던 방식으로 유저 밑에 프로필 을 붙여서 보여주려고 이를 상속받음
     model = Profile
-    con_delete = False  
+    con_delete = False 
 
 class CustomUserAdmin(UserAdmin):
     # fieldsets : 관리자 리스트 화면에서 출력될 폼 설정 부분
-    UserAdmin.fieldsets[1][1]['fields']+=('name', 'school', 'student_num', 'phone_num', 'gender', 'account_num', 'is_mento',)
+    UserAdmin.fieldsets[1][1]['fields']+=('name', 'school', 'phone_num', 'gender', 'is_mento', 'mento_computing', 'mento_basicC', 'mento_GC')
     # add_fieldsets : User 객체 추가 화면에 출력될 입력 폼 설정 부분
     UserAdmin.add_fieldsets += (
-        (('Additional Info'),{'fields':('account_num','student_num')}),
+        (('Additional Info'),{'fields':('account_num','student_num', 'is_mento', 'mento_computing', 'mento_basicC', 'mento_GC')}),
     )
     inlines = (ProfileInline,)
 
