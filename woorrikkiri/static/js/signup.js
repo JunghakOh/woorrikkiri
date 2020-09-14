f46ce3add8bfe3467489a19facbb7754cb3e812a
majung2 <dlalwjd1417@likelion.org>
2020-09-14 14:14:21 +0900

    add signup - 비밀번호확인

--- woorrikkiri/static/js/signup.js
+++ woorrikkiri/static/js/signup.js
@@ -1,11 +1,16 @@
 var pass1 = document.querySelector('#id_password1');
 var pass2 = document.querySelector('#id_password2');
 
+
+const container = document.querySelector('#id_name');
 const button = document.querySelector('#button');
 
-button.addEventListener('click', function(){
+// 비밀번호 일치 확인
+pass2.addEventListener('change', function(){
     if (pass1 != pass2){
         alert('비밀번호가 일치하지 않습니다.');
+        pass1.value = '';
+        pass2.value = '';
     }
     
 })
