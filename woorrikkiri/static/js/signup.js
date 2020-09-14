df4579fdf50012301dede58a7e57d74c932e8a38
majung2 <dlalwjd1417@likelion.org>
2020-09-14 19:04:19 +0900

    update detail, signup

--- woorrikkiri/static/js/signup.js
+++ woorrikkiri/static/js/signup.js
@@ -7,7 +7,7 @@ const button = document.querySelector('#button');
 
 // 비밀번호 일치 확인
 pass2.addEventListener('change', function(){
-    if (pass1 != pass2){
+    if (pass1.value != pass2.value){
         alert('비밀번호가 일치하지 않습니다.');
         pass1.value = '';
         pass2.value = '';
